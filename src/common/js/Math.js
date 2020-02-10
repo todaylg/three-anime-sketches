bde7907c6316ad662eace01a4df4aef1f6a588e2
todaylg <lg2lugang@gmail.com>
2020-02-10 23:07:20 +0800

    Updates

--- src/common/js/Math.js
+++
@@ -1,9 +0,0 @@
-function symmetryRandom() {
-	return Math.random() * 2.0 - 1.0;
-}
-
-
-export {
-    symmetryRandom
-}
-    
--- src/pages/index/index.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import App from './index.vue';
-import VueObserveVisibility from 'vue-observe-visibility';
-
-Vue.use(VueObserveVisibility);
-
-new Vue({
-	render: h => h(App)
-}).$mount('#container');
